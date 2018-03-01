97bab3bb45a49acfd1e85fb2adec48f020f84e27
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2018-03-01 12:57:31 +0530

    get data

--- NCS.PaperGeneration.Entities/CompactEntities/ChoiceCompact.cs
+++ NCS.PaperGeneration.Entities/CompactEntities/ChoiceCompact.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NCS.PaperGeneration.Entities.CommonEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace NCS.PaperGeneration.Entities.CompactEntities
 {
-    public class ChoiceCompact
+    public class ChoiceCompact :  AuditableEntity<int>
     {
         public int QuestionId { get; set; }
     }
