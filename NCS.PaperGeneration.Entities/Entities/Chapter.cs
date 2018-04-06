22e3895bdf39336097b0791871867dd65cb9a941
Chathuranga.Sandun <Chathurangasandunkumara@gmail.com>
2018-04-06 23:11:57 +0530

    fix recursive loop of entities

--- NCS.PaperGeneration.Entities/Entities/Chapter.cs
+++ NCS.PaperGeneration.Entities/Entities/Chapter.cs
@@ -7,9 +7,18 @@ using System.Threading.Tasks;
 
 namespace NCS.PaperGeneration.Entities.Entities
 {
+    using Newtonsoft.Json;
+
     public class Chapter : AuditableEntity<int>
     {
+        public Chapter()
+        {
+            this.Sections = new HashSet<Section>();
+        }
+
         public string ChapterName { get; set; }
         public string ChapterDescription { get; set; }
+        
+        public virtual  ICollection<Section> Sections { get; set; }
     }
 }
